261c0c228ce6e1f447ce2929a54f7766b4f8c32f
luis ortiz <11ortizluis@gmail.com>
2021-11-15 15:30:50 -0500

    api, falta doc

--- parcial 2/parcial2.py
+++ parcial 2/parcial2.py
@@ -3,12 +3,11 @@
 # que manipule estos datos.
 
 import csv,random
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template, jsonify
 import pymongo
 
 # Establece conexion con servidor MongoDB
 con = pymongo.MongoClient("mongodb://localhost:27017/")
-
 db = con["parcial2"] # Base de datos
 col = db["bancovac"] # Coleccion
 
@@ -29,19 +28,14 @@ with open('parcial 2\API_SH.IMM.MEAS_DS2_en_csv_v2_3165446\API_SH.IMM.MEAS_DS2_e
 
 data = dict(zip(titulo[:3], pan[:3])) | dict(zip(titulo[-1], pan[-1]))
 
-
 def agg(b,c):
     a = random.randint(0,99999)
     palabra = { "_id": a, "key": b, "value": c}
     col.insert_one(palabra)
 
-
 def res(a):
     query = col.find_one({"key": a})
     return query
-
-
-
 
 @app.route('/')
 def menu():
@@ -52,22 +46,22 @@ def menu():
             agg(b,c)
         sentinel+=1
     return render_template("menu.html")
+
+@app.route('/api/info/',  methods=['GET'])
+def info():
+    return jsonify(dict(zip(titulo[:3], pan[:3])) )
  
-@app.route('/api/<int:number>/',  methods=['GET', 'POST'])
+@app.route('/api/año/<int:number>/',  methods=['GET'])
 def vacunas(number):
     query = res(str(number))
     return jsonify(query)
 
-@app.route('/listado',  methods=['GET', 'POST'])
-def listado(): 
-   return render_template("listado.html", diccion=col.find({},{"_id": 0, "key": 1, "value": 1}))
-
-@app.route('/buscar',  methods=['GET', 'POST'])
-def buscar():
-    yr = str(request.form.get("palabra"))
-    resultado = res(yr)
-    return render_template("buscar.html", b=yr, x=resultado)
-
+@app.route('/api/datos/',  methods=['GET'])
+def datos():
+    dk = {}
+    for i in col.find({},{"_id": 0, "key": 1, "value": 1}):
+        dk[i["key"]] = i["value"]
+    return jsonify(dk)
 
 if __name__ == '__main__':
     
